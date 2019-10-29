4b060d9a6be048cf378cbe20cd42130cd6dc5f07
Leejunhyuck <hg00521@naver.com>
2019-10-29 23:41:55 +0900

    DP

---
+++ Algorithm/src/Baek11057.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baek11057 {
+
+	
+public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[n+1][10];
+		
+		for(int i=0;i<10;i++) {
+			
+			arr[1][i]=1;
+		}
+		int mod = 10007;
+		for(int i=2;i<=n;i++) {
+			
+			
+			for(int j=0;j<=9;j++) {
+				for(int k=0;k<=j;k++) {
+					arr[i][j]+=arr[i-1][k];
+					arr[i][j] %=mod;
+					
+				}
+				
+			}
+		}
+		int sum=0;
+		for(int i=0;i<=9;i++) {
+			sum =(sum+arr[n][i])%mod;
+			
+		}
+		
+		
+		bw.write(sum+"\n");
+		
+		bw.flush();
+		br.close();
+		bw.close();
+		
+		
+	}
+	
+}
--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -9,36 +9,16 @@ import java.util.*;
 public class Main {
 	
 	
-	public static void main(String[] args) throws IOException {
-			
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			
-			
-			int num = Integer.parseInt(br.readLine());
-			int d[]= new int[1001];//1000으로 하면 오류남
-			Arrays.fill(d, -1);
-			d[0]=0;
-			d[1]=1;
-			d[2]=3;
-			
-			for(int i=3;i<=num;i++) {
-				d[i]=d[i-1]+d[i-2]*2;
-				d[i] %= 10007;
-				
-			}
-			bw.write(String.valueOf(d[num]));
-			
+public static void main(String[] args) throws IOException {
 		
-			
-			
-			bw.flush();
-			br.close();
-			bw.close();
-			
-			
-		}
+	
+	BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+
+	
 		
+	}
 
 	}
 	
